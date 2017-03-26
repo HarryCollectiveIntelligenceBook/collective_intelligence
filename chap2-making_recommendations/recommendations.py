c2a8ed52511e357e43a57f549755298b235066c1
DESKTOP-5IPP58Q\harry <harry.j.tennent@gmail.com>
2017-03-26 12:31:33 +0100

    Collecting preferences-Finding users-Euclidean distance score
    Used the Euclidian distance score to calculate the similarity between
    critics of their opinions, 1 being the same and 0 being no common
    ratings.

--- chap2-making_recommendations/recommendations.py
+++ chap2-making_recommendations/recommendations.py
@@ -1,8 +1,10 @@
-#dicitionary of movie reviewers and ratings for some films
+from math import sqrt
+
+#dictionary of movie reviewers and ratings for some films
 critics = { 'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane':3.5,
 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
 'The Night Listener': 3.0},
-'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane':3.0,
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane':3.5,
 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
 'You, Me and Dupree': 3.5},
 'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane':3.0,
@@ -16,3 +18,22 @@ critics = { 'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane':3.5,
 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
 'Toby': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0,
 'Superman Returns': 4.0}}
+
+#Returns a distance-based similarity score from person1 and person2
+def sim_distance(prefs, person1, person2):
+
+    #Get the list of shared_items
+    si = {}
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item] = 1
+
+    #if they have no ratings in common, return 0
+    if len(si) == 0:
+        return 0
+
+    #Add up the squares of all the differences
+    sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)\
+     for item in si])
+
+    return (1/(1+sqrt(sum_of_squares)))
