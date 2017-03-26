2807a0d6c02bad6758b0ef5fbfea0893f8c94946
DESKTOP-5IPP58Q\harry <harry.j.tennent@gmail.com>
2017-03-26 16:31:50 +0100

    Collecting preferences-finding users-Pearson correlation score
    Added second function to recommendations.py. Calculates a Pearson
    coefficient to find how similar 2 critics are in rating movies by giving
    a score between -1 and 1.

--- chap2-making_recommendations/recommendations.py
+++ chap2-making_recommendations/recommendations.py
@@ -37,3 +37,35 @@ def sim_distance(prefs, person1, person2):
      for item in si])
 
     return (1/(1+sqrt(sum_of_squares)))
+
+#Returns the Pearson correlation coefficient for the p1 and p2
+def sim_pearson(prefs, p1, p2):
+    #Get the list of mutually rated items
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item] = 1
+
+    #Find the number of elements
+    n=len(si)
+
+    #if they have no ratings in common, return 0
+    if n==0:    return 0
+
+    #Add up all the preferences
+    sum1 = sum([prefs[p1][it] for it in si])
+    sum2 = sum([prefs[p2][it] for it in si])
+    #Sum up all the squares
+    sum1Sq = sum([pow(prefs[p1][it],2) for it in si])
+    sum2Sq = sum([pow(prefs[p2][it],2) for it in si])
+    #Sum up the products
+    pSum = sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+    #Calculate the Pearson score
+    num = pSum-(sum1*sum2/n)
+    den = sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+    if den==0:  return 0
+
+    r=num/den
+
+    return r
